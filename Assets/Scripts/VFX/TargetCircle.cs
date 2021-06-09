a53fe3c776f40057cb013ea2fada34b5f68bc611
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-09 14:08:04 +0300

    Added TargetCircle and Trygonomerty utils

---
+++ Assets/Scripts/VFX/TargetCircle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using CarSumo.Utilities;
+
+namespace Assets.Scripts.VFX
+{
+    public class TargetCircle : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed;
+        [SerializeField] private float _frequency;
+        [SerializeField] private float _amplitude;
+        [SerializeField] private float _bias;
+
+        private Vector3 _actualSize;
+
+        private void OnEnable() => _actualSize = transform.localScale;
+
+        private void Update()
+        {
+            ChangeSize();
+            Rotate();
+        }
+
+        private void ChangeSize()
+        {
+            var newAxisSize = GetSizeMultiplier() * _actualSize.x;
+            transform.localScale = new Vector3(newAxisSize, _actualSize.y, newAxisSize);
+        }
+
+        private void Rotate()
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * _rotationSpeed);
+        }
+
+        private float GetSizeMultiplier()
+        {
+            return Mathf.Abs(
+                Trigonometry.CosHarmonicMotion(Time.time, _amplitude, _frequency, 0) + _bias);
+        }
+    }
+}
