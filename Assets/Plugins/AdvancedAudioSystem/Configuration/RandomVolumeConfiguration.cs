a9663cd59175a5ad9a46be8f3d4c501188535634
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-18 12:28:44 +0300

    Added Extra Configurations

---
+++ Assets/Plugins/AdvancedAudioSystem/Configuration/RandomVolumeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace AdvancedAudioSystem.Configuration
+{
+    public class RandomVolumeConfiguration : IAudioConfiguration
+    {
+        [SerializeField, Range(0f, 1f)] private readonly float _minVolume = 1f;
+        [SerializeField, Range(0f, 1f)] private readonly float _maxVolume = 1f;
+
+        public void ApplyTo(AudioSource audioSource)
+        {
+            audioSource.volume = Random.Range(_minVolume, _maxVolume);
+        }
+    }
+}
