70c903d4509475d6c6d11c668e279b60804a0561
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-18 22:59:29 +0300

    Imported Grenade Sounds

---
+++ Assets/Scripts/Units/UnitAudio.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using AdvancedAudioSystem;
+using AdvancedAudioSystem.Emitters;
+
+namespace CarSumo.Units
+{
+    public class UnitAudio : SerializedMonoBehaviour
+    {
+        [SerializeField] private ISoundEmitter _soundEmitter;
+
+        [SerializeField] private AudioCue _destroyCue;
+
+        private Unit _unit;
+
+        private void OnEnable()
+        {
+            _unit = GetComponent<Unit>();
+            _unit.Destroying += PlayDestroySound;
+        }
+
+        private void OnDisable()
+        {
+            _unit.Destroying -= PlayDestroySound;
+        }
+
+        private void PlayDestroySound(Unit unit)
+        {
+            _soundEmitter.Play(_destroyCue);
+        }
+    }
+}
