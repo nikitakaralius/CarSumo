f6e5c0e132d079f018f283b5380ea460135bf6bf
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-18 12:20:34 +0300

    Created AudioCue

---
+++ Assets/Plugins/AdvancedAudioSystem/AudioCue.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using AdvancedAudioSystem.Configuration;
+
+namespace AdvancedAudioSystem
+{
+    [CreateAssetMenu(fileName = "AudioCue", menuName = "Audio/AudioCue")]
+    public class AudioCue : SerializedScriptableObject
+    {
+        [SerializeField] private AudioClipsGroup _audioClips;
+        [SerializeField] private readonly IAudioConfigurationEnumerable _configurations;
+        public AudioClip Clip => _audioClips.NextClip();
+
+        public void ApplyConfiguration(AudioSource source)
+        {
+            foreach (var configuration in _configurations)
+            {
+                configuration.ApplyTo(source);
+            }
+        }
+    }
+}
