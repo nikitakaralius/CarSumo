9246e7a1892b7c7f582ae525de86e8b2d09b01d1
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-19 01:10:36 +0300

    Added Upgrade Sounds

---
+++ Assets/Scripts/Units/UnitAudio.cs
@@ -0,0 +1,35 @@
+﻿using AdvancedAudioSystem;
+using AdvancedAudioSystem.Emitters;
+using UnityEngine;
+
+namespace CarSumo.Units
+{
+    [RequireComponent(typeof(Unit))]
+    public class UnitAudio : MonoBehaviour
+    {
+        [SerializeField] private MonoSoundEmitter _soundEmitter;
+        [SerializeField] private AudioCue _upgradeCue;
+
+        private Unit _unit;
+
+        private void Awake()
+        {
+            _unit = GetComponent<Unit>();
+        }
+
+        private void OnEnable()
+        {
+            _unit.Upgraded += PlayUpgradeSound;
+        }
+
+        private void OnDisable()
+        {
+            _unit.Upgraded -= PlayUpgradeSound;
+        }
+
+        private void PlayUpgradeSound()
+        {
+            _soundEmitter.Play(_upgradeCue);
+        }
+    }
+}
