de1054af6cea0e1e83c69c4b3fbb83ec467f3f7a
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-18 11:12:06 +0300

    Added Audio Clips List

---
+++ Assets/Plugins/AdvancedAudioSystem/AudioClips.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace AdvancedAudioSystem
+{
+    public class AudioClips : List<AudioClip>
+    {
+        public int LastClipPlayed { get; private set; } = -1;
+
+        public AudioClips() : this(0) { }
+
+        public AudioClips(int capacity) : base(capacity) { }
+    }
+
+    public sealed class AudioClipsMember
+    {
+        public AudioClip Clip => _clips[Index];
+
+        public int Index { get; }
+
+        private readonly AudioClips _clips;
+
+        public AudioClipsMember(AudioClips clips, int index)
+        {
+            _clips = clips;
+            Index = index;
+        }
+    }
+}
