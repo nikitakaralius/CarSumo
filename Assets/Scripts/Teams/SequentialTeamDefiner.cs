f24ba9a316b75651f861f77adad0611d4e47002d
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-21 14:10:02 +0300

    Refactored SequentialTeamDefiner

--- Assets/Scripts/Teams/SequentialTeamDefiner.cs
+++ Assets/Scripts/Teams/SequentialTeamDefiner.cs
@@ -4,11 +4,11 @@ namespace CarSumo.Teams
 {
     public class SequentialTeamDefiner : ITeamDefiner
     {
-        private static readonly int _teamsCount = Enum.GetNames(typeof(Team)).Length;
+        private int TeamCount => Enum.GetNames(typeof(Team)).Length;
 
         public Team DefineTeam(Team current)
         {
-            return (Team) ((int) (current + 1) % _teamsCount);
+            return (Team) ((int) (current + 1) % TeamCount);
         }
     }
 }
