df35fb65ce9bbf10f3a174b91cd90ffa32daef60
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-21 21:03:38 +0300

    Started Refactor Unit Enhancer

--- Assets/Scripts/Processors/UnitEnhancer.cs
+++ Assets/Scripts/Processors/UnitEnhancer.cs
@@ -2,14 +2,16 @@
 using CarSumo.Teams;
 using CarSumo.Units;
 using CarSumo.Units.Stats;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace CarSumo.Processors
 {
-    public class UnitEnhancer : MonoBehaviour
+    public class UnitEnhancer : SerializedMonoBehaviour
     {
         [SerializeField] private VehicleDestroyer _destroyer;
         [SerializeField] private VehicleSelector _selector;
+        [SerializeField] private ITeamDefiner _upgradeTeamDefiner = new SequentialTeamDefiner();
 
         private void OnEnable()
         {
@@ -23,8 +25,10 @@ namespace CarSumo.Processors
 
         private void OnVehicleDestroying(IVehicleStatsProvider destroyingEntity)
         {
-            var upgradeTeam = new SequentialTeamDefiner().DefineTeam(destroyingEntity.GetStats().Team);
-            var vehicleToUpgrade = _selector.LastActingVehicles[(int)upgradeTeam];
+            var entityTeam = destroyingEntity.GetStats().Team;
+            var teamToUpgrage = _upgradeTeamDefiner.DefineTeam(entityTeam);
+
+            var vehicleToUpgrade = _selector.LastActingVehicles[(int)teamToUpgrage];
 
             if (vehicleToUpgrade is null)
                 return;
