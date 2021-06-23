8041e8c3be2b0a1d3bb16c779b1352550a108c3e
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-23 21:32:25 +0300

    Added Vehicle Hierarchy

---
+++ Assets/Scripts/Vehicles/Factory/VehicleHierarchy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace CarSumo.Vehicles.Factory
+{
+    [CreateAssetMenu(fileName = "Vehicle Hierarchy", menuName = "CarSumo/Vehicles/Hierarchy")]
+    public class VehicleHierarchy : ScriptableObject
+    {
+        [SerializeField] private VehicleFactory[] _factoriesHierarchy;
+
+        public VehicleFactory GetVehicleFactoryByGeneration(int generation)
+        {
+            if (TryGetVehicleFactoryByGeneration(generation, out var factory) == false)
+                throw new System.ArgumentOutOfRangeException(nameof(generation));
+
+            return factory;
+        }
+
+        public bool TryGetVehicleFactoryByGeneration(int generation, out VehicleFactory factory)
+        {
+            factory = null;
+
+            if (generation >= _factoriesHierarchy.Length || generation < 0)
+                return false;
+
+            factory = _factoriesHierarchy[generation];
+            return true;
+        }
+    }
+}
