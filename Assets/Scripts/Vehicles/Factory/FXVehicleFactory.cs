ccfee25306e43745aa0b4d307894acdcf38912e1
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-06-23 21:25:26 +0300

    Created Vehicle Factory

---
+++ Assets/Scripts/Vehicles/Factory/FXVehicleFactory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using CarSumo.VFX;
+
+namespace CarSumo.Vehicles.Factory
+{
+    [CreateAssetMenu(fileName = "[Vehilce] {FX} factory", menuName = "CarSumo/Vehicles/FXFactory")]
+    public class FXVehicleFactory : VehicleFactory
+    {
+        [SerializeField] private ParticlesFactory _instantiateParticles;
+
+        public override Vehicle Create(Transform parent = null)
+        {
+            _instantiateParticles.Create(parent).Play();
+            return base.Create(parent);
+        }
+    }
+}
---
+++ Assets/Scripts/Vehicles/Factory/VehicleFactory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using CarSumo.Factory;
+
+namespace CarSumo.Vehicles.Factory
+{
+    [CreateAssetMenu(fileName = "[Vehilce] factory", menuName = "CarSumo/Vehicles/Factory")]
+    public class VehicleFactory : FactoryScriptableObject<Vehicle>
+    {
+        [SerializeField] private Vehicle _vehiclePrefab;
+
+        public override Vehicle Create(Transform parent = null)
+        {
+            return Instantiate(_vehiclePrefab, parent);
+        }
+    }
+}
