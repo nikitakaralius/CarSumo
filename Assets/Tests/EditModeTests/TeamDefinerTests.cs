3ec7d0c40576540ac438c432073f659c32da6fd0
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-02 19:13:28 +0300

    Removed Unused Usings

--- Assets/Tests/EditModeTests/TeamDefinerTests.cs
+++ Assets/Tests/EditModeTests/TeamDefinerTests.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using CarSumo.Teams;
 using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
 
 public class TeamDefinerTests
 {
