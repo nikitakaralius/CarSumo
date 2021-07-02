8945b229dc958f68f2fc2248524525f874b65b5a
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-02 18:53:47 +0300

    Added new Tests in the TeamDefinerTests

--- Assets/Tests/EditModeTests/TeamDefinerTests.cs
+++ Assets/Tests/EditModeTests/TeamDefinerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using CarSumo.Teams;
@@ -13,6 +14,41 @@ public class TeamDefinerTests
         var definer = new SequentialTeamDefiner();
 
         Assert.AreEqual(definer.DefineTeam(Team.First), Team.Second);
+    }
+
+    [Test]
+    public void PreviousSequentialTeamDefiner_DetermineNextTeamCorrect()
+    {
+        var definer = new PreviousSequentialTeamDefiner();
+
         Assert.AreEqual(definer.DefineTeam(Team.Second), Team.First);
+    }
+
+    [Test]
+    public void SequentialTeamDefiner_NextTeamAfterSecondIsNotFirst_IfTeamsMoreThanTwo()
+    {
+        var definer = new SequentialTeamDefiner();
+
+        if (Enum.GetNames(typeof(Team)).Length <= 2)
+        {
+            Assert.Pass();
+            return;
+        }
+
+        Assert.AreNotEqual(definer.DefineTeam(Team.Second), Team.First);
+    }
+
+    [Test]
+    public void PreviousSequentialTeamDefiner_PreviousTeamAfterFirstIsNotSecond_IfTeamsMoreThanTwo()
+    {
+        var definer = new SequentialTeamDefiner();
+
+        if (Enum.GetNames(typeof(Team)).Length <= 2)
+        {
+            Assert.Pass();
+            return;
+        }
+
+        Assert.AreNotEqual(definer.DefineTeam(Team.First), Team.Second);
     }
 }
