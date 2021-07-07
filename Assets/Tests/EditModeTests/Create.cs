5f9062fcf50abff8a67e2d3ae0e4d195257fa113
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-07 15:41:10 +0300

    Refactored Editor Mode Tests

---
+++ Assets/Tests/EditModeTests/Create.cs
@@ -0,0 +1,14 @@
+﻿using CarSumo.Teams;
+
+public static class Create
+{
+    public static SequentialTeamDefiner SequentialTeamDefiner()
+    {
+        return new SequentialTeamDefiner();
+    }
+
+    public static PreviousSequentialTeamDefiner PreviousSequentialTeamDefiner()
+    {
+        return new PreviousSequentialTeamDefiner();
+    }
+}
