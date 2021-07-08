66f2ee8514ff388af695371be5951dd9935b9523
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-08 12:05:48 +0300

    Created ProjectContext, Updated Scripts.asmdef

---
+++ Assets/Infrastructure/Insallers/BootstrapInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Infrastructure.Installers
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
