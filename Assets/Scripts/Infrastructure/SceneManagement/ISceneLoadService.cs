3ccef3c56f3b1c46898dcabae5ae63e70113b761
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-11 23:00:40 +0300

    Created SceneLoadService

---
+++ Assets/Scripts/Infrastructure/SceneManagement/ISceneLoadService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CarSumo.Infrastructure.SceneManagement
+{
+    public interface ISceneLoadService
+    {
+        Task Load(SceneLoadData sceneLoadData);
+        Task Unload(SceneLoadData sceneLoadData);
+    }
+}
---
+++ Assets/Scripts/Infrastructure/SceneManagement/SceneLoadData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace CarSumo.Infrastructure.SceneManagement
+{
+    public readonly struct SceneLoadData
+    {
+        public SceneLoadData(string name, LoadSceneMode loadSceneMode)
+        {
+            Name = name;
+            LoadSceneMode = loadSceneMode;
+        }
+        
+        public string Name { get; }
+        
+        public LoadSceneMode LoadSceneMode { get; }
+    }
+}
