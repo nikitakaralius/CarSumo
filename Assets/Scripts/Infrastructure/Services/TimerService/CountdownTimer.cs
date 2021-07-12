de08bbe835ff652ef8919c99168d82d805bfe95f
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-12 23:38:56 +0300

    Created CountdownTimer

---
+++ Assets/Scripts/Infrastructure/Services/TimerService/CountdownTimer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CarSumo.Infrastructure.Services.TimerService
+{
+    public class CountdownTimer : ITimerService
+    {
+        private readonly float _startTimeRemaining;
+        private bool _stopped = false;
+
+        public CountdownTimer(float startTimeRemaining)
+        {
+            _startTimeRemaining = startTimeRemaining;
+        }
+
+        public event Action Elapsed;
+
+        public float Seconds { get; private set; }
+
+        public void Start()
+        {
+            Task.Run(async () =>
+            {
+                _stopped = false;
+                Seconds = _startTimeRemaining;
+
+                while (Seconds > 0.0f && _stopped == false)
+                {
+                    Seconds = Mathf.Clamp(Seconds - Time.deltaTime, 0.0f, _startTimeRemaining);
+                }
+
+                if (Seconds == 0.0f)
+                    Elapsed?.Invoke();
+            });
+        }
+
+        public void Stop()
+        {
+            _stopped = true;
+            Seconds = 0.0f;
+        }
+    }
+}
