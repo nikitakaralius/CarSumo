d2683147bd418ee6d03cd4b08e2d924a5552b3e2
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-19 23:28:46 +0300

    Created GameSettings

---
+++ Assets/Scripts/GameSettings/Structs/AllSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace CarSumo.GameSettings.Structs
+{
+    public class AllSettings
+    {
+        public SoundSettings Sound { get; }
+        public LocalizationSettings Localization { get; }
+
+        public AllSettings(SoundSettings sound, LocalizationSettings localization)
+        {
+            Sound = sound;
+            Localization = localization;
+        }
+    }
+}
