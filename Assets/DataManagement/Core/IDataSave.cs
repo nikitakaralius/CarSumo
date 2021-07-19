f279b3c0f26fba14fc907ac640ab59616b140cb5
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-19 23:38:14 +0300

    Created DataManagement Core Interfaces

---
+++ Assets/DataManagement/Core/IDataSave.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace CarSumo.DataManagement.Core
+{
+    public interface IDataSave
+    {
+        void Save<TModel>(TModel model, string path);
+        Task SaveAsync<TModel>(TModel model, string path);
+    }
+}
