8814b504e08226983db544d080f4b2c76ee530dc
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-20 16:45:04 +0300

    Designed AudioPreferences. Should refactor it later

---
+++ Assets/Scripts/Audio/Services/AudioPreferences.cs
@@ -0,0 +1,47 @@
+﻿using CarSumo.Calculations;
+using CarSumo.GameSettings.Services;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Audio;
+using Zenject;
+
+namespace CarSumo.Audio.Services
+{
+    public class AudioPreferences : MonoBehaviour
+    {
+        [SerializeField] private AudioMixerGroup _musicGroup;
+        [SerializeField] private AudioMixerGroup _sfxGroup;
+        
+        private const string Volume = "Volume";
+        private SettingsService _settings;
+
+        [Inject]
+        private void Construct(SettingsService settingsService)
+        {
+            _settings = settingsService;
+            
+            _musicGroup.audioMixer.SetFloat("MusicVolume", MapVolume(_settings.StoredData.Sound.MusicVolume));
+            _sfxGroup.audioMixer.SetFloat("SfxVolume", MapVolume(_settings.StoredData.Sound.SfxVolume));
+
+        }
+        
+        public void ChangeMusicVolume(float volume)
+        {
+            _musicGroup.audioMixer.SetFloat("MusicVolume", MapVolume(volume));
+            _settings.StoredData.Sound.MusicVolume = volume;
+            _settings.Save();
+        }
+        
+        public void ChangeSfxVolume(float volume)
+        {
+            _musicGroup.audioMixer.SetFloat("SfxVolume", MapVolume(volume));
+            _settings.StoredData.Sound.SfxVolume = volume;
+            _settings.Save();
+        }
+
+        private float MapVolume(float volume)
+        {
+            return Map.MapByRanges(volume, 0, 1, -80, 0);
+        }
+    }
+}
