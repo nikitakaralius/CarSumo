c1346522e89ec002111bf8f1dbfc621ada8e36c9
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-20 23:32:35 +0300

    Created SelectedPlayerPanel

---
+++ Assets/Scripts/GUI/Other/SelectedPlayerPanel.cs
@@ -0,0 +1,32 @@
+﻿using CarSumo.Players;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CarSumo.GUI.Other
+{
+    public class SelectedPlayerPanel : MonoBehaviour
+    {
+        [SerializeField] private GamePlayersRepository _repository;
+
+        [Header("Components")]
+        [SerializeField] private TMP_Text _name;
+        [SerializeField] private Image _icon;
+
+        private void OnEnable()
+        {
+            _repository.SelectedPlayerProfileChanged += OnSelectedPlayerProfileChanged;
+        }
+
+        private void OnDisable()
+        {
+            _repository.SelectedPlayerProfileChanged -= OnSelectedPlayerProfileChanged;
+        }
+
+        private void OnSelectedPlayerProfileChanged()
+        {
+            _name.text = _repository.SelectedPlayerProfile.Name;
+            _icon.sprite = _repository.SelectedPlayerProfile.Icon;
+        }
+    }
+}
--- Assets/Scripts/Players/GamePlayersRepository.cs
+++ Assets/Scripts/Players/GamePlayersRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CarSumo.Players.Models;
 using DataManagement.Players.Models;
@@ -12,23 +13,46 @@ namespace CarSumo.Players
         private IPlayersRepository _repository;
         private IPlayerProfileBuilder _profileBuilder;
 
+        private List<PlayerProfile> _playerProfiles;
+
         [Inject]
         private void Construct(IPlayersRepository playersRepository, IPlayerProfileBuilder profileBuilder)
         {
             _repository = playersRepository;
             _profileBuilder = profileBuilder;
         }
-        
-        public IReadOnlyList<PlayerProfile> PlayerProfiles { get; private set; }
+
+        public event Action SelectedPlayerProfileChanged;
+
+        public IReadOnlyList<PlayerProfile> PlayerProfiles => _playerProfiles;
         public PlayerProfile SelectedPlayerProfile { get; private set; }
 
         private void Awake()
         {
-            PlayerProfiles = BuildPlayerProfiles();
+            _playerProfiles = BuildPlayerProfiles();
             SelectedPlayerProfile = BuildSelectedPlayerProfile();
+            SelectedPlayerProfileChanged?.Invoke();
         }
 
-        private IReadOnlyList<PlayerProfile> BuildPlayerProfiles()
+        public bool TryChangeSelectedPlayerProfile(PlayerProfile playerProfile)
+        {
+            int index = _playerProfiles.IndexOf(playerProfile);
+
+            if (index == -1)
+                return false;
+
+            Player newSelectedPlayer = _repository.Players[index];
+            
+            if (_repository.TryMakePlayerSelected(newSelectedPlayer) == false)
+                return false;
+
+            SelectedPlayerProfile = playerProfile;
+            SelectedPlayerProfileChanged?.Invoke();
+
+            return true;
+        }
+
+        private List<PlayerProfile> BuildPlayerProfiles()
         {
             return _repository.Players
                 .Select(repositoryPlayer => _profileBuilder.BuildFrom(repositoryPlayer))
