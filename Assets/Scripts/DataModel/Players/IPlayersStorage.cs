461585f9a979442319fd2fb084408875c902a80f
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-30 12:18:18 +0300

    Created Player Storages

---
+++ Assets/Scripts/DataModel/Players/IPlayersStorage.cs
@@ -0,0 +1,10 @@
+﻿using UniRx;
+
+namespace CarSumo.DataModel.Players
+{
+    public interface IPlayersStorage
+    {
+        IReadOnlyReactiveCollection<UnityPlayer> AllPlayers { get; }
+        IReadOnlyReactiveProperty<UnityPlayer> ActivePlayer { get; }
+    }
+}
---
+++ Assets/Scripts/DataModel/Players/SerializablePlayerStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CarSumo.DataModel.Players
+{
+    public class SerializablePlayerStorage
+    {
+        public IEnumerable<SerializablePlayer> AllPlayers { get; set; }
+        
+        public SerializablePlayer ActivePlayer { get; set; }
+    }
+}
