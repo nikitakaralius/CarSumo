214800bb17d70c263b7d67e7059f549f96d3da9a
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-30 15:35:29 +0300

    Removed Zenject from GameData

--- Assets/Scripts/DataModel/GameData/Vehicles/IVehicleLayoutFactory.cs
+++ Assets/Scripts/DataModel/GameData/Vehicles/IVehicleLayoutFactory.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections.Generic;
 using DataModel.Vehicles;
-using Zenject;
 
 namespace DataModel.GameData.Vehicles
 {
-    public interface IVehicleLayoutFactory : IFactory<IVehicleLayout>
+    public interface IVehicleLayoutFactory
     {
         IVehicleLayout Create(IEnumerable<VehicleId> vehicles);
     }
