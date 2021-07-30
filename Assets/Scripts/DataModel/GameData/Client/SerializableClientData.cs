78d309a753b3609ad28acbb3333e2d116e768192
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-07-30 14:54:07 +0300

    Created SerializableClientData

---
+++ Assets/Scripts/DataModel/GameData/Client/SerializableClientData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using CarSumo.DataModel.Accounts;
+using CarSumo.DataModel.GameResources;
+using DataModel.Vehicles;
+
+namespace DataModel.GameData.Client
+{
+    public class SerializableClientData
+    {
+        public SerializableAccountStorage AccountStorage { get; set; }
+        
+        public IReadOnlyDictionary<ResourceId, (int, int?)> Resources { get; set; }
+        
+        public IEnumerable<VehicleId> BoughtVehicles { get; set; }
+    }
+}
