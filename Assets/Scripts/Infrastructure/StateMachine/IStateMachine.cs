8116a7d67a17cb7c310c61e279fabf6fe423b775
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-02 11:52:06 +0300

    Created DiInstantiation

--- Assets/Scripts/Infrastructure/StateMachine/IStateMachine.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CarSumo.Infrastructure.StateMachine
-{
-    public interface IStateMachine
-    {
-        void Enter<TState>() where TState : IState;
-        void Register(IState state);
-    }
-}
