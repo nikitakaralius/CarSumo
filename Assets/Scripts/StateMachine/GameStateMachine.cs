ad361affa368ca713a34aaceb5504cafd53f1acd
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-02 11:39:42 +0300

    Added GameStateMachine

---
+++ Assets/Scripts/StateMachine/GameStateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarSumo.StateMachine
+{
+    public class GameStateMachine
+    {
+        private readonly Dictionary<Type, IState> _states;
+        private IState _activeState;
+
+        public GameStateMachine() : this(new Dictionary<Type, IState>()) { }
+
+        public GameStateMachine(IEnumerable<IState> states) : this(GenerateStatesFromEnumerable(states)) { }
+
+        public GameStateMachine(Dictionary<Type, IState> states)
+        {
+            _states = states;
+            _activeState = new IState.Empty();
+        }
+
+        public void Enter<TState>() where TState : IState
+        {
+            _activeState.Exit();
+            _activeState = _states[typeof(TState)];
+            _activeState.Enter();
+        }
+
+        public void Register(IState state)
+        {
+            Register(_states, state);
+        }
+
+        private static Dictionary<Type, IState> GenerateStatesFromEnumerable(IEnumerable<IState> states)
+        {
+            var generatedStates = new Dictionary<Type, IState>();
+            
+            foreach (IState state in states)
+                Register(generatedStates, state);
+
+            return generatedStates;
+        }
+
+        private static void Register(IDictionary<Type, IState> states, IState state)
+        {
+            Type stateKey = state.GetType();
+            states.Add(stateKey, state);
+        }
+    }
+}
--- Assets/Scripts/StateMachine/IState.cs
+++ Assets/Scripts/StateMachine/IState.cs
@@ -2,6 +2,13 @@
 {
     public interface IState
     {
-        
+        void Enter();
+        void Exit();
+
+        public class Empty : IState
+        {
+            public void Enter() { }
+            public void Exit() { }
+        }
     }
 }
