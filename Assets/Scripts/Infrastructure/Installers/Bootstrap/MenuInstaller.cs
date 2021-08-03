4fa0b8b7dcf4b946abe2ac8dc9c31d94a5aabb2a
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-03 13:39:09 +0300

    Fixed All Code Bugs

---
+++ Assets/Scripts/Infrastructure/Installers/Bootstrap/MenuInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Installers.SubContainers;
+using Zenject;
+
+namespace Infrastructure.Installers.Bootstrap
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            ProcessSubContainers();
+        }
+
+        private void ProcessSubContainers()
+        {
+            InstantiationInstaller.Install(Container);
+        }
+    }
+}
