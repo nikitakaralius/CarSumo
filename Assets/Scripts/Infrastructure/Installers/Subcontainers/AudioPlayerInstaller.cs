443bad49ebe0b761541ac4a6af38132e2fccb19d
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-05 13:05:36 +0300

    Created AudioPlayerInstaller

---
+++ Assets/Scripts/Infrastructure/Installers/Subcontainers/AudioPlayerInstaller.cs
@@ -0,0 +1,24 @@
+﻿using AdvancedAudioSystem;
+using UnityEngine;
+using Zenject;
+
+namespace Infrastructure.Installers.SubContainers
+{
+    public class AudioPlayerInstaller : MonoInstaller
+    {
+        [SerializeField] private MonoAudioCuePlayer _audioCuePlayer;
+
+        public override void InstallBindings()
+        {
+            BindAudioPlayer();
+        }
+        
+        private void BindAudioPlayer()
+        {
+            Container
+                .Bind<IAudioPlayer>()
+                .FromInstance(_audioCuePlayer)
+                .AsSingle();
+        }
+    }
+}
