7f0b26ed8c83f3832780d2684109087ced71c040
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-05 15:30:53 +0300

    Created TweenAnimations Framework

---
+++ Assets/Scripts/TweenAnimations/Core/TweenData.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace TweenAnimations
+{
+    [System.Serializable]
+    public class TweenData<T>
+    {
+        public T From;
+        public T To;
+
+        public float Duration;
+        public Ease Ease;
+    }
+    
+    [System.Serializable]
+    public class FloatTweenData : TweenData<float> { }
+    
+    [System.Serializable]
+    public class Vector3TweenData : TweenData<Vector3> { }
+    
+    [System.Serializable]
+    public class Vector2TweenData : TweenData<Vector2> { }
+}
---
+++ Assets/Scripts/TweenAnimations/LoopedSizeTweenAnimation.cs
@@ -0,0 +1,33 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace TweenAnimations
+{
+    public class LoopedSizeTweenAnimation : MonoBehaviour, ITweenAnimation
+    {
+        [SerializeField] private Vector3TweenData _data;
+        [SerializeField] private bool _rememberInitialScale = true;
+        
+        private Tweener _tweener;
+
+        private void Start()
+        {
+            if (_rememberInitialScale)
+                _data.From = transform.localScale;
+        }
+
+        public void Play()
+        {
+            _tweener = transform
+                .DOScale(_data.To, _data.Duration)
+                .SetEase(_data.Ease)
+                .SetLoops(-1, LoopType.Yoyo)
+                .OnKill(() => transform.localScale = _data.From);
+        }
+
+        public void Stop()
+        {
+            _tweener?.Kill();
+        }
+    }
+}
