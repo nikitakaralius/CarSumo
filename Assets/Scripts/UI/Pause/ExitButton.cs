9fac0294caddfb5a91d7a64d442273b480fbb4f1
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-05 14:18:59 +0300

    Created ExitButton

---
+++ Assets/Scripts/UI/Pause/ExitButton.cs
@@ -0,0 +1,27 @@
+﻿using CarSumo.StateMachine;
+using CarSumo.StateMachine.States;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI.Pause
+{
+    [RequireComponent(typeof(Button))]
+    public class ExitButton : MonoBehaviour
+    {
+        private GameStateMachine _stateMachine;
+
+        [Inject]
+        private void Construct(GameStateMachine stateMachine)
+        {
+            _stateMachine = stateMachine;
+        }
+
+        private void Start()
+        {
+            GetComponent<Button>()
+                .onClick
+                .AddListener(_stateMachine.Enter<MenuEntryState>);
+        }
+    }
+}
