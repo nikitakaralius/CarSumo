7569dc1764be138b7334a7cb99d4fe36e5526369
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-06 16:16:04 +0300

    Added sounds to the change vehicle buttons

---
+++ Assets/Scripts/Infrastructure/Installers/Subcontainers/VehicleAssetsProviderInstaller.cs
@@ -0,0 +1,24 @@
+﻿using DataModel.GameData.Vehicles;
+using UnityEngine;
+using Zenject;
+
+namespace Infrastructure.Installers.SubContainers
+{
+    public class VehicleAssetsProviderInstaller : MonoInstaller
+    {
+        [SerializeField] private VehicleAssetsProviderSo _assetsProvider;
+
+        public override void InstallBindings()
+        {
+            BindVehicleAssetsProvider();
+        }
+
+        private void BindVehicleAssetsProvider()
+        {
+            Container
+                .Bind<IVehicleAssetsProvider>()
+                .FromInstance(_assetsProvider)
+                .AsSingle();
+        }
+    }
+}
