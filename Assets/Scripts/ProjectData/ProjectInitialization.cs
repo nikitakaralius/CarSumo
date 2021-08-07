578000cfb3f6240e128e16e0f71d2924cf15ed5f
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-07 21:37:16 +0300

    Moved Some of Infrastructure files to ProjectData

---
+++ Assets/Scripts/ProjectData/ProjectInitialization.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Initialization;
+using Zenject;
+
+namespace Infrastructure
+{
+    public class ProjectInitialization
+    {
+        private readonly DiContainer _container;
+
+        public ProjectInitialization(DiContainer container)
+        {
+            _container = container;
+        }
+
+        public async Task InitializeAsync()
+        {
+            var dataFilesInitialization = _container.Instantiate<DataFilesInitialization>();
+            dataFilesInitialization.Initialize();
+
+            IEnumerable<Type> initializationTypes = GetProjectAsyncInitializationsTypes();
+            IEnumerable<IAsyncInitializable> initializations = CreateProjectInitializations(initializationTypes);
+
+            await Task.WhenAll(initializations.Select(initialization => initialization.InitializeAsync()));
+        }
+
+        private IEnumerable<IAsyncInitializable> CreateProjectInitializations(IEnumerable<Type> types)
+        {
+            return types.Select(type => (IAsyncInitializable)_container.Instantiate(type));
+        }
+
+        private IEnumerable<Type> GetProjectAsyncInitializationsTypes()
+        {
+            Type initializableBaseType = typeof(IAsyncInitializable);
+            
+            return AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => type.IsAssignableFrom(initializableBaseType) && type.IsClass);
+        }
+    }
+}
