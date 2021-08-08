beb4725af84bc80322c5545e65481cac488986af
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-08 14:12:50 +0300

    Created CardVehicleLayoutView

---
+++ Assets/Scripts/Menu/Vehicles/Layout/Cards/CardVehicleLayoutView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu.Vehicles.Layout
+{
+    public class CardVehicleLayoutView : VehicleLayoutView<VehicleCard>
+    {
+        [SerializeField] private Transform _layoutRoot;
+        [SerializeField] private Vector3 _cardSize = Vector3.one * 0.75f;
+
+        protected override Transform LayoutRoot => _layoutRoot;
+        
+        protected override void ProcessCreatedLayout(IEnumerable<VehicleCard> layout)
+        {
+            foreach (VehicleCard card in layout)
+            {
+                card.transform.localScale = _cardSize;
+            }
+        }
+    }
+}
