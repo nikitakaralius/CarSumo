8d9be3af54237016c8b5e623d8a74468a0b8c57f
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-08 15:43:39 +0300

    Changed VehicleStorageView

--- Assets/Scripts/Menu/Vehicles/Storage/VehicleStorageView.cs
+++ Assets/Scripts/Menu/Vehicles/Storage/VehicleStorageView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using CarSumo.DataModel.Accounts;
 using DataModel.Vehicles;
 using Menu.Vehicles.Layout;
 using UniRx;
@@ -13,32 +15,55 @@ namespace Menu.Vehicles.Storage
         [SerializeField] private Transform _layoutRoot;
 
         private IVehicleStorage _vehicleStorage;
+        private IAccountStorage _accountStorage;
         private IDisposable _storageChangedSubscription;
-        
+
         [Inject]
-        private void Construct(IVehicleStorage vehicleStorage)
+        private void Construct(IVehicleStorage vehicleStorage, IAccountStorage accountStorage)
         {
             _vehicleStorage = vehicleStorage;
+            _accountStorage = accountStorage;
         }
-        
+
         protected override Transform CollectionRoot => _layoutRoot;
 
         private IReadOnlyReactiveCollection<VehicleId> BoughtVehicles => _vehicleStorage.BoughtVehicles;
 
-        private void OnEnable()
+        private IReadOnlyReactiveCollection<VehicleId> Layout =>
+            _accountStorage.ActiveAccount.Value.VehicleLayout.Value.ActiveVehicles;
+
+        private async void OnEnable()
         {
+            await SpawnCollectionAsync(GetVehiclesExceptLayout());
+
             _storageChangedSubscription = BoughtVehicles
                 .ObserveCountChanged()
-                .Subscribe(async _ => await SpawnCollectionAsync(BoughtVehicles));
+                .Subscribe(async _ => await SpawnCollectionAsync(GetVehiclesExceptLayout()));
         }
 
         private void OnDisable()
         {
-            _storageChangedSubscription.Dispose();   
+            _storageChangedSubscription.Dispose();
         }
 
         protected override void ProcessCreatedLayout(IEnumerable<VehicleCard> layout)
         {
+        }
+
+        private IEnumerable<VehicleId> GetVehiclesExceptLayout()
+        {
+            List<VehicleId> layout = new List<VehicleId>(Layout);
+
+            foreach (VehicleId vehicle in BoughtVehicles)
+            {
+                if (layout.Any(layoutVehicle => layoutVehicle == vehicle))
+                {
+                    layout.Remove(vehicle);
+                    continue;
+                }
+
+                yield return vehicle;
+            }
         }
     }
 }
