3ff4ed4df5041c188781bacb34563e54012ba40c
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-08 13:40:33 +0300

    Created VehicleLayoutView abstract class

--- Assets/Scripts/Menu/Vehicles/Layout/VehicleLayoutView.cs
+++ Assets/Scripts/Menu/Vehicles/Layout/VehicleLayoutView.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using CarSumo.DataModel.Accounts;
 using DataModel.GameData.Vehicles;
@@ -14,14 +13,17 @@ using Zenject;
 
 namespace Menu.Vehicles.Layout
 {
-    public class VehicleLayoutView : SerializedMonoBehaviour
+    public abstract class VehicleLayoutView<T> : SerializedMonoBehaviour where T : Component
     {
-        [SerializeField] private IVehicleAssetsProvider _cardAssets;
+        [SerializeField] private IVehicleAssetsProvider _assetsProvider;
 
         private IAccountStorage _accountStorage;
         private IAsyncInstantiation _instantiation;
 
-        private CompositeDisposable _disposables = new CompositeDisposable();
+        private List<T> _items = new List<T>();
+
+        private IDisposable _accountChangedSubscription;
+        private IDisposable _layoutChangedSubscription;
         
         [Inject]
         private void Construct(IAccountStorage accountStorage, IAsyncInstantiation instantiation)
@@ -30,28 +32,48 @@ namespace Menu.Vehicles.Layout
             _instantiation = instantiation;
         }
 
+        public IReadOnlyList<T> Items => _items;
+        private IReadOnlyReactiveProperty<Account> ActiveAccount => _accountStorage.ActiveAccount;
+        private IReadOnlyReactiveProperty<IVehicleLayout> Layout => _accountStorage.ActiveAccount.Value.VehicleLayout;
+
         private void OnEnable()
         {
-            
+            _accountChangedSubscription = ActiveAccount.Subscribe(OnActiveAccountChanged);
         }
 
         private void OnDisable()
         {
-            
+            _layoutChangedSubscription?.Dispose();
+            _accountChangedSubscription?.Dispose();
         }
 
-        private async Task CreateLayout(IVehicleLayout layout)
+        public abstract Task SpawnLayoutAsync(IVehicleLayout layout);
+
+        public void Clear()
         {
-            IEnumerable<AssetReferenceGameObject> layoutAssets = GetLayoutAssets(layout.ActiveVehicles);
-            foreach (AssetReferenceGameObject asset in layoutAssets)
+            foreach (T vehicle in _items)
             {
-                
+                Destroy(vehicle.gameObject);
             }
         }
 
-        private IEnumerable<AssetReferenceGameObject> GetLayoutAssets(IEnumerable<VehicleId> vehicles)
+        private void OnActiveAccountChanged(Account account)
         {
-            return vehicles.Select(id => _cardAssets.GetAssetByVehicleId(id));
+            _layoutChangedSubscription?.Dispose();
+            _layoutChangedSubscription = Layout.Subscribe(async layout => await SpawnLayoutAsync(layout));
+        }
+
+        private async Task<IEnumerable<T>> GetVehicleItemsAsync(IEnumerable<VehicleId> vehicleIds, Transform parent)
+        {
+            var vehicles = new List<T>();
+            foreach (VehicleId vehicleId in vehicleIds)
+            {
+                AssetReferenceGameObject asset = _assetsProvider.GetAssetByVehicleId(vehicleId);
+                T vehicleItem = await _instantiation.InstantiateAsync<T>(asset, parent);
+                vehicles.Add(vehicleItem);
+            }
+            
+            return vehicles;
         }
     }
 }
