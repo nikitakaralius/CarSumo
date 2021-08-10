33d52c320399fbf8302b2a4c431a087edf8b521c
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-10 13:55:42 +0300

    Created raw VehicleCard logic (need to refactor)

--- Assets/Scripts/Menu/Vehicles/Cards/VehicleCard.cs
+++ Assets/Scripts/Menu/Vehicles/Cards/VehicleCard.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdvancedAudioSystem;
 using DataModel.Vehicles;
+using Sirenix.Utilities;
 using TweenAnimations;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,8 +19,15 @@ namespace Menu.Vehicles.Cards
 
         private IAudioPlayer _audioPlayer;
 
+        private Transform _contentRoot;
+        private Transform _selectedRoot;
+        private LayoutGroup _layoutGroup;
+        private IEnumerable<VehicleCard> _layout;
+
         private Button _button;
+        
         private bool _clicked = false;
+        private int _siblingIndex;
 
         public VehicleId Id => _id;
 
@@ -27,12 +37,39 @@ namespace Menu.Vehicles.Cards
             _audioPlayer = audioPlayer;
         }
 
-        public void Initialize(Action<bool> onButtonClicked)
+        public void Initialize(Transform contentRoot,
+                                Transform selectedRoot,
+                                LayoutGroup layoutGroup,
+                                Action<bool> onButtonClicked,
+                                IEnumerable<VehicleCard> layout)
         {
+            _layout = layout;
+            _contentRoot = contentRoot;
+            _selectedRoot = selectedRoot;
+            _layoutGroup = layoutGroup;
+
             _button ??= GetComponent<Button>();
 
             _button.onClick.RemoveAllListeners();
             _button.onClick.AddListener(() => OnButtonClickedInternal(onButtonClicked));
+        }
+
+        private void OnDisable()
+        {
+            _clicked = false;
+            _layoutGroup.enabled = true;
+            transform.SetParent(_contentRoot);
+            transform.SetSiblingIndex(_siblingIndex);
+            _animation.DecreaseSize();
+        }
+
+        private void SetInactive()
+        {
+            _clicked = false;
+            
+            transform.SetParent(_contentRoot);
+            transform.SetSiblingIndex(_siblingIndex);
+            _animation.DecreaseSize();
         }
         
         private void OnButtonClickedInternal(Action<bool> onButtonClicked)
@@ -42,9 +79,30 @@ namespace Menu.Vehicles.Cards
             onButtonClicked.Invoke(_clicked);
 
             if (_clicked)
+            {
+                _siblingIndex = transform.GetSiblingIndex();
+
+                _layoutGroup.enabled = false;
+                transform.SetParent(_selectedRoot);
+                
                 _animation.IncreaseSize();
+                NotifyOtherCardsBeingClicked();
+            }
             else
+            {
+                _layoutGroup.enabled = true;
+                transform.SetParent(_contentRoot);
+                transform.SetSiblingIndex(_siblingIndex);
+                
                 _animation.DecreaseSize();
+            }
+        }
+
+        private void NotifyOtherCardsBeingClicked()
+        {
+            _layout
+                .Where(x => x != this)
+                .ForEach(x => x.SetInactive());
         }
     }
 }
--- Assets/Scripts/Menu/Vehicles/Layout/Cards/CardVehicleLayoutView.cs
+++ Assets/Scripts/Menu/Vehicles/Layout/Cards/CardVehicleLayoutView.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using Menu.Vehicles.Cards;
+﻿using Menu.Vehicles.Cards;
 using Menu.Vehicles.Storage;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu.Vehicles.Layout
 {
     public class CardVehicleLayoutView : VehicleLayoutView<VehicleCard>
     {
         [SerializeField] private Transform _layoutRoot;
+        [SerializeField] private LayoutGroup _layoutGroup;
         [SerializeField] private VehicleStorageView _storage;
         [SerializeField] private Vector3 _cardSize = Vector3.one * 0.75f;
-
-        public event Action Canceled;
-
+        
         protected override Transform CollectionRoot => _layoutRoot;
         
         protected override void ProcessCreatedLayout(IEnumerable<VehicleCard> layout)
@@ -21,7 +20,7 @@ namespace Menu.Vehicles.Layout
             foreach (VehicleCard card in layout)
             {
                 card.transform.localScale = _cardSize;
-                card.Initialize(OnLayoutVehicleCardClicked);
+                card.Initialize(_layoutRoot, transform, _layoutGroup, OnLayoutVehicleCardClicked, layout);
             }
         }
 
