7fab02f356f219fc16ae7068e9ee23ccc6b200b4
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-10 12:46:52 +0300

    Created Vehicle Card Animation

--- Assets/Scripts/Menu/Vehicles/Layout/Cards/CardVehicleLayoutView.cs
+++ Assets/Scripts/Menu/Vehicles/Layout/Cards/CardVehicleLayoutView.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using Menu.Vehicles.Cards;
+using Menu.Vehicles.Storage;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Menu.Vehicles.Layout
 {
     public class CardVehicleLayoutView : VehicleLayoutView<VehicleCard>
     {
-        [SerializeField] private LayoutGroup _layoutGroup;
         [SerializeField] private Transform _layoutRoot;
+        [SerializeField] private VehicleStorageView _storage;
         [SerializeField] private Vector3 _cardSize = Vector3.one * 0.75f;
+
+        public event Action Canceled;
 
         protected override Transform CollectionRoot => _layoutRoot;
         
@@ -17,6 +21,19 @@ namespace Menu.Vehicles.Layout
             foreach (VehicleCard card in layout)
             {
                 card.transform.localScale = _cardSize;
+                card.Initialize(OnLayoutVehicleCardClicked);
+            }
+        }
+
+        private void OnLayoutVehicleCardClicked(bool clicked)
+        {
+            if (clicked)
+            {
+                _storage.Enable();
+            }
+            else
+            {
+                _storage.Disable();
             }
         }
     }
--- Assets/Scripts/Menu/Vehicles/Storage/VehicleStorageView.cs
+++ Assets/Scripts/Menu/Vehicles/Storage/VehicleStorageView.cs
@@ -3,9 +3,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using CarSumo.DataModel.Accounts;
 using DataModel.Vehicles;
+using Menu.Vehicles.Cards;
 using UniRx;
 using UnityEngine;
-using UnityEngine.UI;
 using Zenject;
 
 namespace Menu.Vehicles.Storage
@@ -13,7 +13,6 @@ namespace Menu.Vehicles.Storage
     public class VehicleStorageView : VehicleCollectionView<VehicleCard>
     {
         [SerializeField] private Transform _layoutRoot;
-        [SerializeField] private LayoutGroup _layoutGroup;
 
         private IVehicleStorage _vehicleStorage;
         private IAccountStorage _accountStorage;
@@ -30,24 +29,38 @@ namespace Menu.Vehicles.Storage
 
         private IReadOnlyReactiveCollection<VehicleId> BoughtVehicles => _vehicleStorage.BoughtVehicles;
 
-        private IVehicleLayout Layout =>
-            _accountStorage.ActiveAccount.Value.VehicleLayout.Value;
+        private IReactiveProperty<IVehicleLayout> Layout =>
+            _accountStorage.ActiveAccount.Value.VehicleLayout;
 
         private void OnEnable()
         {
             BoughtVehicles
                 .ObserveCountChanged()
-                .Subscribe(async _ => await SpawnPreparedCollectionAsync(Layout))
+                .Subscribe(async _ => await SpawnPreparedCollectionAsync(Layout.Value))
                 .AddTo(_subscriptions);
 
-            _accountStorage.ActiveAccount
-                .Subscribe(async account => await SpawnPreparedCollectionAsync(account.VehicleLayout.Value))
+            //_accountStorage.ActiveAccount
+            //    .Subscribe(async account => await SpawnPreparedCollectionAsync(account.VehicleLayout.Value))
+            //    .AddTo(_subscriptions);
+
+            Layout
+                .Subscribe(async layout => await SpawnPreparedCollectionAsync(layout))
                 .AddTo(_subscriptions);
         }
 
         private void OnDisable()
         {
             _subscriptions.Dispose();
+        }
+
+        public void Enable()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Disable()
+        {
+            gameObject.SetActive(false);
         }
 
         protected override void ProcessCreatedLayout(IEnumerable<VehicleCard> layout)
