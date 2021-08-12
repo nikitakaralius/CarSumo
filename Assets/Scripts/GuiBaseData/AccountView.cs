ee5f05feff9257bd65ac69a3e37654a0433e14b4
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-12 23:55:52 +0300

    Created UiAccountPresenter

---
+++ Assets/Scripts/GuiBaseData/AccountView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CarSumo.DataModel.Accounts;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuiBaseData.Accounts
+{
+    public abstract class AccountView : MonoBehaviour
+    {
+        [Header("Account View")]
+        [SerializeField] private TMP_Text _name;
+        [SerializeField] private Image _icon;
+
+        public void ChangeAccount(Account account)
+        {
+            _name.text = account.Name.Value;
+            _icon.sprite = account.Icon.Value.Sprite;
+        }
+
+        protected void ChangeViewValues(string name, Sprite icon)
+        {
+	        _name.text = name;
+	        _icon.sprite = icon;
+        }
+    }
+}
