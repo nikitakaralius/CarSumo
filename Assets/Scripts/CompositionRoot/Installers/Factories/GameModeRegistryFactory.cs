0806a343ac170edd98768fc68e1b835bd14328a7
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-12 21:15:49 +0300

    Created GameModeRegistryFactory

---
+++ Assets/Scripts/CompositionRoot/Installers/Factories/GameModeRegistryFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CarSumo.DataModel.Accounts;
+using CarSumo.Teams;
+using GameModes;
+using Zenject;
+
+namespace Infrastructure.Installers.Factories
+{
+	public class GameModeRegistryFactory : IFactory<GameModeRegistry>
+	{
+		private readonly IAccountStorage _accountStorage;
+
+		public GameModeRegistryFactory(IAccountStorage accountStorage)
+		{
+			_accountStorage = accountStorage;
+		}
+		
+		public GameModeRegistry Create()
+		{
+			return new GameModeRegistry(RegisterInitialAccounts());
+		}
+
+		private IReadOnlyDictionary<Team, Account> RegisterInitialAccounts()
+		{
+			Account blueTeamAccount = _accountStorage.ActiveAccount.Value;
+			Account redTeamAccount = _accountStorage.AllAccounts
+				.FirstOrDefault(account => account.Equals(blueTeamAccount) == false);
+			
+			return new Dictionary<Team, Account>
+			{
+				{Team.Blue, blueTeamAccount},
+				{Team.Red, redTeamAccount}
+			};
+		}
+	}
+}
--- Assets/Scripts/Menu/GameModes/Common/Accounts/SelectedAccountView.cs
+++ Assets/Scripts/Menu/GameModes/Common/Accounts/SelectedAccountView.cs
@@ -1,15 +1,22 @@
-﻿using AdvancedAudioSystem;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdvancedAudioSystem;
 using CarSumo.DataModel.Accounts;
 using CarSumo.Teams;
+using GameModes;
 using Menu.Accounts;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace Menu.GameModes.Common.Accounts
 {
 	[RequireComponent(typeof(Button))]
-	public class SelectedAccountView : AccountView
+	public class SelectedAccountView : AccountView, IAccountListRules, IAccountSelectHandler
 	{
+		[Header("Selected Account View Preferences")]
 		[SerializeField] private Team _team;
 		[SerializeField] private AccountListView _accountList;
 
@@ -17,7 +24,61 @@ namespace Menu.GameModes.Common.Accounts
 
 		private IAccountStorage _accountStorage;
 		private IAudioPlayer _audioPlayer;
+
+		private IGameModePreferences _gameModePreferences;
+		private IGameModeOperations _gameModeOperations;
+
+		private IDisposable _changeAccountSubscription;
+
+		[Inject]
+		private void Construct(IAccountStorage storage, IAudioPlayer player, IGameModePreferences preferences,
+			IGameModeOperations operations)
+		{
+			_accountStorage = storage;
+			_audioPlayer = player;
+			_gameModePreferences = preferences;
+			_gameModeOperations = operations;
+		}
+
+		public bool SelectActivated => false;
+
+		public IEnumerable<Account> AccountsToRender
+			=> _accountStorage.AllAccounts.Where(account => account.Equals(_selectedAccount) == false);
+
+		private void OnEnable()
+		{
+			_changeAccountSubscription = _gameModePreferences
+									.GetAccountByTeam(_team)
+									.Subscribe(ChangeAccount);
+		}
+
+		private void Start()
+		{
+			Button button = GetComponent<Button>();
+			
+			button.onClick.AddListener(() =>
+			{
+				_audioPlayer.Play();
+				_accountList.Open(this, this);
+			});
+		}
+
+		private void OnDisable()
+		{
+			_changeAccountSubscription?.Dispose();
+		}
 		
+		public void OnButtonSelected(AccountListItem element)
+		{
+			_gameModeOperations.RegisterAccount(_team, element.Account);
+			_accountList.Close();
+		}
+
+		public void OnButtonDeselected(AccountListItem element)
+		{
+			
+		}
+
 		protected override void OnAccountChanged(Account account)
 		{
 			_selectedAccount = account;
