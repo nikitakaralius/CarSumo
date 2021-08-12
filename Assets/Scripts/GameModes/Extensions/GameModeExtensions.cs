a2d3214a6cfcb06c27b1ad8b8830ba82e07d715f
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-12 23:19:54 +0300

    Created GameModeExtensions

---
+++ Assets/Scripts/GameModes/Extensions/GameModeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CarSumo.Teams;
+
+namespace GameModes.Extensions
+{
+	public static class GameModeExtensions
+	{
+		public static bool CanEnterGameMode(this IGameModePreferences gameModePreferences, params Team[] teamsToCheck)
+		{
+			return teamsToCheck.All(team => gameModePreferences.GetAccountByTeam(team).HasValue);
+		}
+		
+		public static bool CanEnterGameMode(this IGameModePreferences gameModePreferences, IEnumerable<Team> teamsToCheck)
+		{
+			return teamsToCheck.All(team => gameModePreferences.GetAccountByTeam(team).HasValue);
+		}
+	}
+}
