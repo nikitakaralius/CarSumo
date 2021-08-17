aafd8cdd60a7cc079939e15b43310378fea0e64d
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-17 13:19:41 +0300

    Removed Old Vehicle logic. Created IVehicleStats abstraction

---
+++ Assets/Scripts/Vehicles/Stats/IVehicleStats.cs
@@ -0,0 +1,14 @@
+﻿using CarSumo.Teams;
+using UnityEngine;
+
+namespace CarSumo.Vehicles.Stats
+{
+	public interface IVehicleStats
+	{
+		Team Team { get; }
+	
+		AnimationCurve NormalizedDrivingSpeed { get; }
+		
+		float DrivingTime { get; }
+	}
+}
--- Assets/Scripts/Vehicles/Stats/IVehicleStatsProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using CarSumo.Teams;
-
-namespace CarSumo.Vehicles.Stats
-{
-    public interface IVehicleStatsProvider
-    {
-        VehicleStats GetStats();
-    }
-}
