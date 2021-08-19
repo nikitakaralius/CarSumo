ceb2f80a592bbd0bba9dcbf41dfdff0175b168e0
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-20 00:20:14 +0300

    Created AccountExceptionPopup

---
+++ Assets/Scripts/Menu/Accounts/AccountExceptionPopup/AccountExceptionPopup.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Menu.Accounts.AccountExceptionPopup
+{
+	public class AccountExceptionPopup : MonoBehaviour, IAccountExceptionPopup
+	{
+		[Header("Required components")] 
+		[SerializeField] private TMP_Text _exceptionMessage;
+		
+		public void Show(string exceptionMessage)
+		{
+			_exceptionMessage.text = exceptionMessage;
+		}
+	}
+}
