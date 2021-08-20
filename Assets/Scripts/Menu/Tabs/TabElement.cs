0115d674d0fa824b54348b753f08888f44ecf77a
nikitakaralyus <73189234+nikitakaralyus@users.noreply.github.com>
2021-08-20 15:33:10 +0300

    Created Tabs

---
+++ Assets/Scripts/Menu/Tabs/TabElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu.Tabs
+{
+	public class TabElement : MonoBehaviour
+	{
+		[Header("Required Components")]
+		[SerializeField] private TMP_Text _title;
+		[SerializeField] private Button _button;
+		[SerializeField] private GameObject _tabFocus;
+
+		[Header("Preferences")] 
+		[SerializeField] private Color _selectedColor = Color.white;
+		[SerializeField] private Color _defaultColor = Color.white;
+		[SerializeField] private bool _selectedOnStart = false;
+
+		private readonly Subject<TabElement> _selectedObserver = new Subject<TabElement>();
+		
+		private void OnValidate() => SetSelected(_selectedOnStart);
+
+		private void Start()
+		{
+			_button.onClick.AddListener(() =>
+			{
+				SetSelected(true);
+				_selectedObserver.OnNext(this);
+			});
+		}
+
+		public void SetSelected(bool selected)
+		{
+			_title.color = selected ? _selectedColor : _defaultColor;
+			_tabFocus.gameObject.SetActive(selected);
+		}
+
+		public IObservable<TabElement> ObserveSelected() => _selectedObserver;
+	}
+}
