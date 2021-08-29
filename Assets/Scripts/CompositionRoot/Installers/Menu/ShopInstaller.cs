0f23726c6a04185b6c4d16ad30109081175c8d86
nikitakaralyus <nikitakaralyus@gmail.com>
2021-08-29 21:47:28 +0300

    Created ExceptionMessage bingings

--- Assets/Scripts/CompositionRoot/Installers/Menu/ShopInstaller.cs
+++ Assets/Scripts/CompositionRoot/Installers/Menu/ShopInstaller.cs
@@ -1,4 +1,5 @@
 ﻿using Shop;
+using Shop.ExceptionMessaging;
 using UnityEngine;
 using Zenject;
 
@@ -7,17 +8,24 @@ namespace Infrastructure.Installers.Menu
 	public class ShopInstaller : MonoInstaller
 	{
 		[SerializeField] private ShopWindow _sceneShopWindow;
+		[SerializeField] private ExceptionMessage _sceneExceptionMessage;
+		
 		
 		public override void InstallBindings()
 		{
 			BindShopWindow();
+			BindExceptionMessage();
 		}
 
-		private void BindShopWindow()
-		{
+		private void BindShopWindow() =>
 			Container
 				.BindInstance(_sceneShopWindow)
 				.AsSingle();
-		}
+
+		private void BindExceptionMessage() =>
+			Container
+				.BindInterfacesAndSelfTo<ExceptionMessage>()
+				.FromInstance(_sceneExceptionMessage)
+				.AsSingle();
 	}
 }
