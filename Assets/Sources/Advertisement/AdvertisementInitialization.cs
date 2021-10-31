233f65a5d9f5dcfd0d35e64834e2ef01472c0362
nikitakaralyus <nikitakaralyus@gmail.com>
2021-10-31 18:22:47 +0400

    Created AdvertisementInitialization

---
+++ Assets/Sources/Advertisement/AdvertisementInitialization.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Advertisement
+{
+	public class AdvertisementInitialization : MonoBehaviour
+	{
+		[SerializeField] private AdvertisementPreferences _preferences;
+		
+		private void OnEnable()
+		{
+			IronSource.Agent.init(_preferences.AppKey);
+			IronSource.Agent.validateIntegration();
+		}
+	}
+}
