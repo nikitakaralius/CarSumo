c7c6430b244a344efa49026c6b63faf529e9f0c3
nikitakaralyus <nikitakaralyus@gmail.com>
2021-10-31 18:13:41 +0400

    Created AdvertisementLifecycle

---
+++ Assets/Sources/Advertisement/AdvertisementLifecycle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Advertisement
+{
+	public class AdvertisementLifecycle : MonoBehaviour
+	{
+		private void OnEnable()
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+
+		private void OnApplicationPause(bool pauseStatus)
+		{
+			IronSource.Agent.onApplicationPause(pauseStatus);
+		}
+	}
+}
