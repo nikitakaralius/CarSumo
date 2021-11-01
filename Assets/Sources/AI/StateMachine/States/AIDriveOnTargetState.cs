1f7eacae3ea51cf525155e9b70c7d16971b8d825
nikitakaralyus <nikitakaralyus@gmail.com>
2021-11-01 15:05:37 +0400

    Created AiDriveOnTargetState

---
+++ Assets/Sources/AI/StateMachine/States/AIDriveOnTargetState.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using AI.StateMachine.Common;
+using AI.Structures;
+using CarSumo.Vehicles;
+using UnityEngine;
+
+namespace AI.StateMachine.States
+{
+	public class AIDriveOnTargetState : IAsyncState
+	{
+		private readonly PairTransfer _transfer;
+
+		public AIDriveOnTargetState(PairTransfer transfer)
+		{
+			_transfer = transfer;
+		}
+
+		private Vector3 TargetDirection => _transfer.Pair.Direction * -1;
+		private Vehicle ControlledVehicle => _transfer.Pair.Controlled;
+
+		public Task DoAsync()
+		{
+			ControlledVehicle.Rotation.RotateBy(TargetDirection);
+			ControlledVehicle.Engine.SpeedUp(1);
+			
+			return Task.CompletedTask;
+		}
+	}
+}
