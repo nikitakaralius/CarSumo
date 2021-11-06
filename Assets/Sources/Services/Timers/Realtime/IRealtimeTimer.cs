0a2e493277ff3c7bed0d0a3bfcbca19120265233
nikitakaralyus <nikitakaralyus@gmail.com>
2021-11-06 15:37:06 +0400

    Created OfflineRealtimeTimer

---
+++ Assets/Sources/Services/Timers/Realtime/IRealtimeTimer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UniRx;
+
+namespace Services.Timers.Realtime
+{
+	public interface IRealtimeTimer
+	{
+		event Action Elapsed;
+		IReadOnlyReactiveProperty<DateTime> TimeLeft { get; }
+	}
+
+	public interface IRealtimeTimerOperations
+	{
+		void Start(DateTime duration);
+	}
+}
---
+++ Assets/Sources/Services/Timers/Realtime/OfflineRealtimeTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using CarSumo.Coroutines;
+using UniRx;
+
+namespace Services.Timers.Realtime
+{
+	public class OfflineRealtimeTimer : IRealtimeTimer, IRealtimeTimerOperations
+	{
+		private readonly ReactiveProperty<DateTime> _timeLeft;
+		private readonly CoroutineExecutor _executor;
+
+		public OfflineRealtimeTimer(DateTime timeLeft, CoroutineExecutor executor)
+		{
+			_timeLeft = new ReactiveProperty<DateTime>(timeLeft);
+			_executor = executor;
+		}
+
+		public event Action Elapsed;
+
+		public IReadOnlyReactiveProperty<DateTime> TimeLeft => _timeLeft;
+
+		private DateTime Time => _timeLeft.Value;
+
+		public void Start(DateTime duration)
+		{
+			_executor.StartCoroutine(TickRoutine());
+		}
+
+		private IEnumerator TickRoutine()
+		{
+			while (Time > DateTime.MinValue)
+			{
+				_timeLeft.Value = Time.AddSeconds(-UnityEngine.Time.deltaTime);
+				yield return null;
+			}
+			
+			Elapsed?.Invoke();
+			_timeLeft.Value = DateTime.MinValue;
+		}
+	}
+}
