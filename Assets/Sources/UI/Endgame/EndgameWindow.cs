03d82f84be53d0275fb63e3f57410b9fe885374e
nikitakaralyus <nikitakaralyus@gmail.com>
2021-11-15 18:59:01 +0400

    Created Endgame window

---
+++ Assets/Sources/UI/Endgame/EndgameWindow.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Sources.UI.Endgame
+{
+	public class EndgameWindow : MonoBehaviour
+	{
+		[SerializeField, Required, SceneObjectsOnly] private GameObject _gameplayElements;
+		
+		public void Show()
+		{
+			_gameplayElements.SetActive(false);
+			gameObject.SetActive(true);
+		}
+	}
+}
