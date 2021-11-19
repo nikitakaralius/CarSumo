59515319d5642233fee8af83a74b157af3da6de2
nikitakaralyus <nikitakaralyus@gmail.com>
2021-11-19 21:29:49 +0400

    Fixed playable bot account bug

---
+++ Assets/Sources/Menu/Hub/MenuBoot.cs
@@ -0,0 +1,23 @@
+﻿using GameModes;
+using GameModes.Extensions;
+using UnityEngine;
+using Zenject;
+
+namespace Menu.Hub
+{
+	public class MenuBoot : MonoBehaviour
+	{
+		private IGameModeOperations _operations;
+
+		[Inject]
+		private void Construct(IGameModeOperations operations)
+		{
+			_operations = operations;
+		}
+
+		private void OnEnable()
+		{
+			_operations.ClearRegistrations();
+		}
+	}
+}
