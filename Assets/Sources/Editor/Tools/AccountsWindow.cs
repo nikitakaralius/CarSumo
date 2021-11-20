92e8709ff0d04673b7d5ee5046e645b7fdb59d76
nikitakaralyus <nikitakaralyus@gmail.com>
2021-11-20 17:38:00 +0400

    Imported NSubstitute

---
+++ Assets/Sources/Editor/Tools/AccountsWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CarSumo.DataModel.Accounts;
+using CarSumo.Teams;
+using UnityEditor;
+using UnityEngine;
+using Zenject;
+
+namespace CustomEditors.Tools
+{
+	public class AccountsWindow : EditorWindow
+	{
+		[MenuItem("Tools/Accounts")]
+		public static AccountsWindow FocusOrCreateWindow() => GetWindow<AccountsWindow>("Accounts");
+
+		private Team _team;
+		
+		private DiContainer GlobalContainer => ProjectContext.Instance.Container;
+
+		private void OnGUI()
+		{
+			DisplayVehicleLayout();
+		}
+
+		private void DisplayVehicleLayout()
+		{
+			using (new GUILayout.HorizontalScope(EditorStyles.helpBox))
+			{
+				bool showVehicleLayoutClicked = GUILayout.Button("Show vehicle layout");
+
+				IEnumerable<Account> accounts = Array.Empty<Account>();
+
+				if (showVehicleLayoutClicked)
+				{
+					var repository = GlobalContainer.TryResolve<IAccountRepository>();
+				}
+			}
+		}
+	}
+}
