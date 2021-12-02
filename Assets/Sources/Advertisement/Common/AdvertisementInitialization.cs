67c429ec60f6e56db4a15b6deaad73584cddd3d5
nikitakaralyus <nikitakaralyus@gmail.com>
2021-12-02 17:41:17 +0400

    Added ironSource integration with UnityAds

--- Assets/Sources/Advertisement/Common/AdvertisementInitialization.cs
+++ Assets/Sources/Advertisement/Common/AdvertisementInitialization.cs
@@ -5,10 +5,15 @@ namespace Advertisement.Common
 	public class AdvertisementInitialization : MonoBehaviour
 	{
 		[SerializeField] private AdvertisementPreferences _preferences;
-		
+
 		private void OnEnable()
 		{
 			IronSource.Agent.init(_preferences.AppKey);
+			IronSource.Agent.setMetaData("UnityAds_coppa", "true");
+			
+#if UNITY_EDITOR
+			IronSource.Agent.setAdaptersDebug(true);
+#endif
 			IronSource.Agent.validateIntegration();
 		}
 	}
