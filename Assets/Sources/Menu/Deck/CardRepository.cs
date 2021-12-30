a2d3ee316eeb08de53dfa35091fdf7c2aea95d34
nikitakaralyus <nikitakaralyus@gmail.com>
2021-12-30 14:20:30 +0400

    Created CardRepository

---
+++ Assets/Sources/Menu/Deck/CardRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataModel.GameData.Vehicles;
+using DataModel.Vehicles;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Menu.Deck
+{
+	[CreateAssetMenu(menuName = "Cards/Repository", fileName = "CardRepository")]
+	public class CardRepository : SerializedScriptableObject, ICardRepository
+	{
+		[SerializeField] private IVehicleAssets _assets;
+		
+		public AssetReferenceGameObject ViewOf(VehicleId id)
+		{
+			return _assets.GetAssetByVehicleId(id);
+		}
+	}
+}
