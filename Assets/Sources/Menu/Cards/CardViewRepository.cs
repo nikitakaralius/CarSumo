f7ad30a3b9fd6a80a1c07587aa39d6e060d4673a
nikitakaralyus <nikitakaralyus@gmail.com>
2021-12-31 21:39:00 +0400

    Created CardViewRepository

---
+++ Assets/Sources/Menu/Cards/CardViewRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using DataModel.Vehicles;
+using UnityEngine;
+
+namespace Menu.Cards
+{
+	[CreateAssetMenu(fileName = "CarViewRepository", menuName = "Cards/CardViewRepository")]
+	public class CardViewRepository : ScriptableObject, ICardViewRepository
+	{
+		[SerializeField] private IReadOnlyDictionary<Vehicle, GameObject> _views = new Dictionary<Vehicle, GameObject>();
+		
+		public GameObject ViewOf(Vehicle vehicle)
+		{
+			if (_views.ContainsKey(vehicle) == false)
+			{
+				throw new ArgumentOutOfRangeException($"{nameof(vehicle)} doesn't registered in the stats repository");
+			}
+			return _views[vehicle];
+		}
+	}
+}
